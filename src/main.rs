c242b875f165d84259340bf86e534a63117b6fbf
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-10-31 22:29:19 +0900

    hello world

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::fs;
+use std::process::exit;
+fn main() {
+    let file_path = "docs.gl/gl4/glClear.xhtml";
+    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: could not read file {file_path} : {err}");
+        exit(1)
+    });
+    println!("{content}");
+}
